7d21ebd2bd6879679a55c207cc8921229378e98c
Kelphils <53217748+Kelphils@users.noreply.github.com>
2021-07-01 22:21:12 +0100

    image allign

--- src/Components/Imagelink/Imagelink.js
+++ src/Components/Imagelink/Imagelink.js
@@ -11,7 +11,7 @@ const ImageLinkForm = ({onInputChange, onPictureSubmit}) => {
                    <div className= 'form pa4 br3 shadow-5'>
                    <input className= 'f4 pa2 w-80 center' type= 'text' onChange={onInputChange} />
                    <button className= 'w-30 grow f4 link ph3 pv2'
-                   style={{color: 'turquoise', backgroundColor: 'white', border: '1px solid gray'}}
+                   style={{color: 'turquoise', backgroundColor: 'white', border: '1px solid gray', textAlign: 'center'}}
                    onClick= {onPictureSubmit}
                     >Detect</button>
                </div>
